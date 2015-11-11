50e7fd7756a3996df2e05316dd92d14f79126ac9
sophiavanvalkenburg <sophia.vanvalkenburg@nytimes.com>
2015-11-10 22:03:38 -0500

    more refactoring textdialogmode

--- js/action.js
+++ js/action.js
@@ -53,13 +53,10 @@ TextDialogMode.prototype.downArrowButtonHandler = function(controller){
 TextDialogMode.prototype.selectButtonHandler = function(controller){
   this.gotoNextMessage(controller);
 };
-TextDialogMode.prototype.setTextDialogMessage = function(controller){
-  controller.setTextDialogMessage(this.environment.messages[0]);
-}
 TextDialogMode.prototype.gotoNextMessage = function(controller){
   if (!this.shouldEndMode()){
     this.environment.messages = this.environment.messages.slice(1);
-    this.setTextDialogMessage(controller);
+    this.initialize(controller);
   }
 }
 TextDialogMode.prototype.shouldEndMode = function(){
@@ -67,7 +64,7 @@ TextDialogMode.prototype.shouldEndMode = function(){
 }
 TextDialogMode.prototype.initialize = function(controller){
   if (!this.shouldEndMode()){
-    this.setTextDialogMessage();
+    controller.setTextDialogMessage(this.environment.messages[0]);
   }
 }
 TextDialogMode.prototype.clear = function(controller){
