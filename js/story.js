66cfa1a86b3b823ae7ee685571fec1fd0239b0c4
sophiavanvalkenburg <sophia.vanvalkenburg@nytimes.com>
2015-11-19 16:04:25 -0500

    refactored story.js

--- js/story.js
+++ js/story.js
@@ -1,22 +1,45 @@
+var StoryEvent = new function(name, event_fn){
+  this.event_fn = event_fn;
+  this.name = name; 
+}
+StoryEvent.prototype.trigger = function(){
+  this.event_fn();
+}
+
 var Story = new function(controller){
   this.controller = controller;
   this.objects = {
     COIN: "coin"
   }
+  this.setupStoryEvents();
 }
-Story.prototype.receiveCoinFromFountain = function(){
-  var player = this.controller.haus.getPlayer();
-  player.addToInventory(this.objects.COIN);
-}
+Story.prototype.setupStoryEvents = function(){
+  var controller = this.controller;
+  var objects = this.objects;
+  this.events = {
 
-Story.prototype.movePlayerAfterReceivingCoin = function(){
-  var player = this.controller.haus.getPlayer();
-  var next_map = this.controller.haus.getMap(0);
-  var next_tile = next_map.getTile(10, 7);
-  player.moveTo(next_tile);
-}
+    receiveCoinFromFountain: 
+    new StoryEvent("receive-coin-from-fountain",
+      function(){
+        var player = controller.haus.getPlayer();
+        player.addToInventory(objects.COIN);
+      }),
 
-Story.prototype.placeCoinOnKitchenTable = function(){
-  var player = this.controller.haus.getPlayer();
-  player.removeFromInventory(StoryObjects.COIN);
+    movePlayerAfterReceivingCoin:
+    new StoryEvent("move-player-after-receiving-coin",
+      function(){
+        var player = controller.haus.getPlayer();
+        var next_map = controller.haus.getMap(0);
+        var next_tile = next_map.getTile(10, 7);
+        player.moveTo(next_tile);
+      }),   
+
+    placeCoinOnKitchenTable:
+    new StoryEvent("place-coin-on-kitchen-table",
+      function(){
+        var player = controller.haus.getPlayer();
+        player.removeFromInventory(objects.COIN);
+      })
+
+  }
 }
