698b82c8aeb68ad2f4e6b6e43468bf6ef95be1d5
sophiavanvalkenburg <sophia.vanvalkenburg@nytimes.com>
2015-11-19 00:26:31 -0500

    add new maps + images

--- js/haus_config.js
+++ js/haus_config.js
@@ -1,4 +1,10 @@
 var HausConfig = {
-  mapfiles: ["resources/maps/map0.json", "resources/maps/map1.json"],
+  mapfiles: [
+    "resources/maps/map0.json", 
+    "resources/maps/map1.json", 
+    "resources/maps/map11.json", 
+    "resources/maps/map12.json", 
+    "resources/maps/map7.json"
+  ],
   player: new PlayerCharacter("Sophia", "resources/images/characters/player.png", 0, 10, 7)
 }
