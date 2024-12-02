5878949bcc0d6f15a7179524ac867ce4811285eb
Sophia van Valkenburg <sophiavanvalkenburg@users.noreply.github.com>
2024-12-02 13:04:35 -0500

    change animation.js to es6

--- js/animation.js
+++ js/animation.js
@@ -1,64 +1,76 @@
-var Animation = function(obj){
-  this.loop = obj.loop === undefined ? true : obj.loop;
-  this.queue = Animation.createAnimationQueue(obj.instructions, obj.length);
-  this.next_instruction = 0;
-}
+class Animation {
 
-Animation.createAnimationQueue = function (instructions, length){
-  var queue = [];
-  for (var i=0; i<instructions.length; i++){
-    var ins = instructions[i];
-    if (ins.wait !== undefined){
-      var wait_queue = Animation.createWaitQueue(ins.wait);
-      Array.prototype.push.apply(queue, wait_queue);
+  #loop;
+  #queue;
+  #next_instruction;
+
+  constructor(obj) {
+    this.#loop = obj.loop === undefined ? true : obj.loop;
+    this.#queue = Animation.createAnimationQueue(obj.instructions, obj.length);
+    this.#next_instruction = 0;
+  }
+
+  static createAnimationQueue(instructions, length) {
+    const queue = [];
+    for (let i=0; i<instructions.length; i++){
+      const ins = instructions[i];
+      if (ins.wait !== undefined){
+        let wait_queue = Animation.createWaitQueue(ins.wait);
+        queue.push(...wait_queue);
+      } else {
+        queue.push(ins);
+      }
+    }
+    if (length !== undefined && queue.length < length){
+      const queue_diff = length - queue.length;
+      const wait_queue = Animation.createWaitQueue(queue_diff);
+      queue.push(...wait_queue);
+    }
+    return queue;
+  }
+
+  static createWaitQueue(length){
+    const wait_queue = [];
+    for (let i=0; i<length; i++){
+      wait_queue.push({ wait: 1 });
+    }
+    return wait_queue;
+  }
+
+  getNextInstruction(){
+    if (this.#next_instruction < 0 || this.#next_instruction >= this.#queue.length){
+      return undefined;
+    }
+    return this.#queue[this.#next_instruction];
+  }
+
+  update(){
+    this.#next_instruction++;
+    if (this.#next_instruction >= this.#queue.length){
+      if (this.#loop){
+        this.#next_instruction = 0;
+      }else{
+        this.#next_instruction = -1;
+      }
+    }
+  }
+
+  getLastOrientation(){
+    if (this.#next_instruction == 0){
+      return undefined;
+    }
+    if (this.#next_instruction < 0 || this.#next_instruction >= this.#queue.length){
+      last_instruction = this.#queue.length - 1;
     }else{
-      queue.push(ins);
+      last_instruction = this.#next_instruction - 1;
+    }
+    for (let i=last_instruction; i>=0; i--){
+      const ins = this.#queue[i];
+      if (ins.orientation !== undefined){
+        return ins.orientation;
+      }
     }
   }
-  if (length !== undefined && queue.length < length){
-    var queue_diff = length - queue.length;
-    var wait_queue = Animation.createWaitQueue(queue_diff);
-    Array.prototype.push.apply(queue, wait_queue);
-  }
-  return queue;
-}
-Animation.createWaitQueue = function(length){
-  var wait_queue = [];
-  for (var i=0; i<length; i++){
-    wait_queue.push({ wait: 1 });
-  }
-  return wait_queue;
-}
-Animation.prototype.getNextInstruction = function(){
-  if (this.next_instruction < 0 || this.next_instruction >= this.queue.length){
-    return undefined;
-  }
-  var ins = this.queue[this.next_instruction];
-  return ins;
-}
-Animation.prototype.update = function(){
-  this.next_instruction++;
-  if (this.next_instruction >= this.queue.length){
-    if (this.loop){
-      this.next_instruction = 0;
-    }else{
-      this.next_instruction = -1;
-    }
-  }
-}
-Animation.prototype.getLastOrientation = function(){
-  if (this.next_instruction == 0){
-    return undefined;
-  }
-  if (this.next_instruction < 0 || this.next_instruction >= this.queue.length){
-    last_instruction = this.queue.length - 1;
-  }else{
-    last_instruction = this.next_instruction - 1;
-  }
-  for (var i=last_instruction; i>=0; i--){
-    var ins = this.queue[i];
-    if (ins.orientation !== undefined){
-      return ins.orientation;
-    }
-  }
+  
+
 }
