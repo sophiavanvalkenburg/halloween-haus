2a469f5d85fcc61608394fdd7fd4523422470d1a
Sophia van Valkenburg <sophiavanvalkenburg@users.noreply.github.com>
2025-04-27 16:02:59 -0400

    convert dialog.js to es6

--- js/dialog.js
+++ js/dialog.js
@@ -1,46 +1,55 @@
-var TextDialog = function(){
-  this.message = undefined;
-};
-TextDialog.prototype.unsetMessage = function(){
-  this.message = undefined;
-}
-TextDialog.prototype.setMessage = function(message){
-  this.message = message;
-};
-TextDialog.prototype.getMessage = function(){
-  return this.message;
-};
-TextDialog.prototype.hasMessage = function(){
-  return this.message !== undefined;
-}
+class TextDialog {
+  
+  #message;
 
+  constructor(){}
 
-var ChoiceDialog = function(){
-  this.choices = [];
-  this.selected = undefined;
-}
-ChoiceDialog.prototype.hasChoices = function(){
-  return this.choices.length !== 0;
-}
-ChoiceDialog.prototype.unsetChoices = function(){
-  this.choices = [];
-}
-ChoiceDialog.prototype.setChoices = function(choices){
-  this.choices = choices;
-}
-ChoiceDialog.prototype.getChoiceLabels = function(){
-  var labels = [];
-  for (var i=0; i<this.choices.length; i++){
-    var choice = this.choices[i];
-    if (choice !== undefined){
-      labels.push(choice);
-    }
+  unsetMessage(){
+    this.#message = undefined;
   }
-  return labels;
+  setMessage(message){
+    this.#message = message;
+  }
+  getMessage(){
+    return this.#message;
+  }
+  hasMessage(){
+    return this.#message !== undefined;
+  }
 }
-ChoiceDialog.prototype.selectChoice = function(item_index){
-  this.selected = item_index;
-}
-ChoiceDialog.prototype.getSelectedLabel = function(){
-  return this.choices[this.selected];
+
+
+class ChoiceDialog {
+
+  #choices;
+  #selected;
+
+  constructor(){
+    this.#choices = [];
+  }
+
+  hasChoices(){
+    return this.#choices.length !== 0;
+  }
+  unsetChoices(){
+    this.#choices = [];
+  }
+  setChoices(choices){
+    this.#choices = choices;
+  }
+  getChoiceLabels(){
+    const labels = [];
+    for (let choice of this.#choices){
+      if (choice !== undefined){
+        labels.push(choice);
+      }
+    }
+    return labels;
+  }
+  selectChoice(item_index){
+    this.#selected = item_index;
+  }
+  getSelectedLabel(){
+    return this.#choices[this.#selected];
+  }
 }
