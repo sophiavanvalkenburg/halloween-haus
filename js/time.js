031fdfcaf45774d7e3964672b5b16953e4503a10
Sophia van Valkenburg <sophiavanvalkenburg@users.noreply.github.com>
2025-04-27 16:41:23 -0400

    convert time.js to es6

--- js/time.js
+++ js/time.js
@@ -1,15 +1,21 @@
-var TimeManager = function(){
-  this.counter_inc = Config.TIME_COUNTER;
-  this.tickEvent = new Event("tick");
-}
+class TimeManager {
+  
+  #counter_inc;
 
-TimeManager.prototype.startCounter = function(){
-  this.stopped = false;
-  var tm = this
-  setTimeout(function(){
-    if (!tm.stopped){
-      this.dispatchEvent(tm.tickEvent);
-      tm.startCounter();
-    }
-  }, this.counter_inc);
+  constructor(){
+    this.#counter_inc = Config.TIME_COUNTER;
+    this.tickEvent = new Event("tick");
+    this.stopped = false;
+  }
+
+  startCounter(){
+    this.stopped = false;
+    const tm = this;
+    setTimeout(() => {
+      if (!tm.stopped){
+        this.dispatchEvent(tm.tickEvent);
+        tm.startCounter();
+      }
+    }, this.#counter_inc);
+  }
 }
