e971b41b48aa3c9b8418050b726b26cf2e9680af
Sophia van Valkenburg <sophiavanvalkenburg@users.noreply.github.com>
2025-04-27 16:34:54 -0400

    convert loader.js to es6

--- js/loader.js
+++ js/loader.js
@@ -1,111 +1,118 @@
-var GameLoader = function(haus){
-  this.haus = haus;
-  this.load_jobs = 0;
-  this.preloaded_graphics = [];
-}
-GameLoader.prototype.loadGame = function(callback){
-  this.preloadGraphicsFromList();
-  this.preloadMapObjects(Config.characters);
-  var mapfile_list = Config.mapfiles;
-  for (var i = 0; i<mapfile_list.length; i++){
-    this.startLoadMap(mapfile_list[i], callback);
-  } 
-}
-GameLoader.prototype.startLoadMap = function(file, callback){
-  this.load_jobs++;
-  the_loader = this;
-  $.getJSON(file, function(json){
-    the_loader.loadMap(json);
-    the_loader.load_jobs--;
-    if (the_loader.load_jobs === 0){
-      callback();
-    }
-  });
-}
-GameLoader.prototype.convertToInt = function(str){
-  var new_int = parseInt(str);
-  if (isNaN(new_int)){
-    return -1;
-  }
-  return new_int;
-}
-GameLoader.prototype.makeTileImageSrc = function(src){
-  var png = src.split("/").slice(-1)[0];
-  return "resources/images/tiles/"+png;
-}
-GameLoader.prototype.parseMetaData = function(data){
-  if (data === undefined || data.meta === undefined){
-    return;
-  }
-  var res = this.convertToInt(data.meta.resolution);
-  var cols = this.convertToInt(data.meta.num_columns);
-  var rows = this.convertToInt(data.meta.num_rows);
-  var map = this.convertToInt(data.meta.map_id);
-  if (res === -1 || cols === -1 || rows === -1 || map === -1){
-    return;
-  }
-  return {resolution: res, num_columns: cols, num_rows: rows, map_id: map, is_ghost_map: data.meta.is_ghost_map};
-}
-GameLoader.prototype.makeMapLocation = function(loc){
-  if (loc === undefined){
-    return;
-  }
-  var map = this.convertToInt(loc.map);
-  var x = this.convertToInt(loc.x);
-  var y = this.convertToInt(loc.y);
-  if (map === -1 || x === -1 || y === -1){
-    return;
-  }
-  return new MapLocation(map, x, y);
-}
-GameLoader.prototype.makeTileModes = function(tile_data){
-  if (tile_data.messages.length > 0){
-    return [
-            {
-              state: StoryStates.INIT,
-              modes: [TextDialogMode.createFactory(tile_data.messages)]
-            }
-        ];
-  } else {
-    return [];
-  }
-}
-GameLoader.prototype.parseTileData = function(tile_data){
-  var loc = this.makeMapLocation(tile_data.loc);
-  var portal = this.makeMapLocation(tile_data.portal);
-  var img_src = this.makeTileImageSrc(tile_data.graphic);
-  var modes = this.makeTileModes(tile_data);
-  return new Tile(tile_data.label, loc, portal, tile_data.is_accessible, img_src, modes);
-}
-GameLoader.prototype.loadMap = function(data){
-  var meta = this.parseMetaData(data);
-  if (meta === undefined){
-    return;
-  }
-  var tiles = [];
-  for (var i=0; i<data.tiles.length; i++){
-    tiles.push(this.parseTileData(data.tiles[i]));
-  }
-  //this.preloadMapObjects(tiles);
-  this.haus.addMap( new MapState(meta.map_id, tiles, meta.is_ghost_map));
-}
-GameLoader.prototype.preloadMapObjects = function(objs){
-  for (var i=0; i<objs.length; i++){
-    var src = objs[i].getGraphic();
-    this.preloadGraphic(src);
-  }
-}
-GameLoader.prototype.preloadGraphicsFromList = function(){
-  for (var i=0; i<Config.preload.length; i++){
-    this.preloadGraphic(Config.preload[i]);
-  }
-}
-GameLoader.prototype.preloadGraphic = function(url){
-  if (Config.DEBUG === true){
-    return;
-  }
-  var graphic = new Image();
-  graphic.src = url;
-  this.preloaded_graphics.push(graphic);
-}
+class GameLoader{
   
+  #haus;
+  #preloaded_graphics;
+
+  constructor(haus){
+    this.#haus = haus;
+    this.load_jobs = 0;
+    this.#preloaded_graphics = [];
+  }
+
+  static convertToInt(str){
+    const new_int = parseInt(str);
+    if (isNaN(new_int)){
+      return -1;
+    }
+    return new_int;
+  }
+  static makeTileImageSrc(src){
+    const png = src.split("/").slice(-1)[0];
+    return `resources/images/tiles/${png}`;
+  }
+  static makeMapLocation(loc){
+    if (loc === undefined){
+      return;
+    }
+    const map = GameLoader.convertToInt(loc.map);
+    const x = GameLoader.convertToInt(loc.x);
+    const y = GameLoader.convertToInt(loc.y);
+    if (map === -1 || x === -1 || y === -1){
+      return;
+    }
+    return new MapLocation(map, x, y);
+  }
+  static makeTileModes(tile_data){
+    if (tile_data.messages.length > 0){
+      return [
+              {
+                state: StoryStates.INIT,
+                modes: [TextDialogMode.createFactory(tile_data.messages)]
+              }
+          ];
+    } else {
+      return [];
+    }
+  }
+  static parseTileData(tile_data){
+    const loc = GameLoader.makeMapLocation(tile_data.loc);
+    const portal = GameLoader.makeMapLocation(tile_data.portal);
+    const img_src = GameLoader.makeTileImageSrc(tile_data.graphic);
+    const modes = GameLoader.makeTileModes(tile_data);
+    return new Tile(tile_data.label, loc, portal, tile_data.is_accessible, img_src, modes);
+  }
+  static parseMetaData(data){
+    if (data === undefined || data.meta === undefined){
+      return;
+    }
+    const res = GameLoader.convertToInt(data.meta.resolution);
+    const cols = GameLoader.convertToInt(data.meta.num_columns);
+    const rows = GameLoader.convertToInt(data.meta.num_rows);
+    const map = GameLoader.convertToInt(data.meta.map_id);
+    if (res === -1 || cols === -1 || rows === -1 || map === -1){
+      return;
+    }
+    return {resolution: res, num_columns: cols, num_rows: rows, map_id: map, is_ghost_map: data.meta.is_ghost_map};
+  }
+
+  loadGame(callback){
+    this.preloadGraphicsFromList();
+    this.preloadMapObjects(Config.characters);
+    for (let mapfile of Config.mapfiles){
+      this.startLoadMap(mapfile, callback);
+    } 
+  }
+  startLoadMap(file, callback){
+    this.load_jobs++;
+    const the_loader = this;
+    $.getJSON(file, (json) => {
+      the_loader.loadMap(json);
+      the_loader.load_jobs--;
+      if (the_loader.load_jobs === 0){
+        callback();
+      }
+    });
+  }
+  loadMap(data){
+    const meta = GameLoader.parseMetaData(data);
+    if (meta === undefined){
+      return;
+    }
+    const tiles = [];
+    for (let tile of data.tiles){
+      tiles.push(GameLoader.parseTileData(tile));
+    }
+    //this.preloadMapObjects(tiles);
+    this.#haus.addMap( new MapState(meta.map_id, tiles, meta.is_ghost_map));
+  }
+  preloadMapObjects(objs){
+    for (let obj of objs){
+      const src = obj.getGraphic();
+      this.preloadGraphic(src);
+    }
+  }
+  preloadGraphicsFromList(){
+    for (let url of Config.preload){
+      this.preloadGraphic(url);
+    }
+  }
+  preloadGraphic(url){
+    if (Config.DEBUG === true){
+      return;
+    }
+    const graphic = new Image();
+    graphic.src = url;
+    this.#preloaded_graphics.push(graphic);
+  }
+  
+}
